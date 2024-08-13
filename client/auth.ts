589e57fd0f3db0af471c96673737729a2f517959
witchbl4des <menzo.smit-adam@epitech.eu>
2024-08-13 09:51:14 +0200

    chore: Refactor auth module exports, add signout user action, and update npm dependencies

--- client/auth.ts
+++ client/auth.ts
@@ -1,42 +1,38 @@
-import NextAuth from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import * as v from "valibot";
-import argon2 from "argon2";
 import { SigninSchema } from "@/validators/signin-validator";
 import { findUserByEmail } from "./resources/user-queries";
+import Credentials from "next-auth/providers/credentials";
+import NextAuth from "next-auth";
+import * as v from "valibot";
+import argon2 from "argon2";
 import { OAuthAccountAlreadyLinkedError } from "@/lib/customErrors";
 import { authConfig } from "./auth.config";
 
-const { providers: authConfigProviders, ...authConfigRest } = authConfig;
+const { providers: authConfigProviders, ...authConfigRest } = authConfig
 
 const nextAuth = NextAuth({
-  ...authConfigRest,
-  providers: [
-    ...authConfigProviders,
-    Credentials({
-      async authorize(credentials) {
-        const parsedCredentials = v.safeParse(SigninSchema, credentials);
+    ...authConfigRest,
+    providers: [
+        ...authConfigProviders,
+        Credentials({
+            async authorize(credentials) {
+                const parsedCredentials = v.safeParse(SigninSchema, credentials);
+                if (parsedCredentials.success) {
+                    const { email, password } = parsedCredentials.output;
+                    const user = await findUserByEmail(email);
+                    if (!user) return null;
+                    if (!user.password) throw new OAuthAccountAlreadyLinkedError();
 
-        if (parsedCredentials.success) {
-          const { email, password } = parsedCredentials.output;
+                    const passwordMatch = await argon2.verify(user.password, password);
+                    if (passwordMatch) {
+                        const { password: _, ...userWithoutPassword} = user;
+                        return userWithoutPassword;
+                    }
+                }
 
-          const user = await findUserByEmail(email);
-          if (!user) return null;
-
-          if (!user.password) throw new OAuthAccountAlreadyLinkedError();
-
-          const passwordsMatch = await argon2.verify(user.password, password);
-
-          if (passwordsMatch) {
-            const { password: _, ...userWithoutPassword } = user;
-            return userWithoutPassword;
-          }
-        }
-
-        return null;
-      },
-    }),
-  ],
+                return null;
+            },
+        }),
+    ]
 });
 
-export const { signIn, signOut, auth, handlers } = nextAuth;
+export const { signIn, auth, signOut, handlers } = nextAuth;
